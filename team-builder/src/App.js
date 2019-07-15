b90da789757dc4221201795e6e90acf8b0d3d2a7
Mona LisaHeyClos <thiscarlos@gmail.com>
2019-07-15 16:47:09 -0700

    forms created

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+
+  const [teamInfo, setTeamInfo] = useState({
+    username: "",
+    password: ""
+  });
+
+  const submitHandler = event => {
+    event.preventDefault();
+    // console.log(username)
+    // console.log(password)
+    // Send data to server
+    // Change data
+    // Go to a new page
+  };
+
+  const changeHandler = event => {
+    console.log(formState);
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value + "i"
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandler}>
+        <label>
+          Username
+          <input
+            name="username"
+            type="text"
+            placeholder="Enter your username"
+            onChange={changeHandler}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            name="password"
+            // type="password"
+            placeholder="Enter your password"
+            value={formState.password}
+            onChange={changeHandler}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
